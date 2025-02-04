83d9aab40f482f820dff26b19f3ddc218c5d2303
Thalys <thalys.gadelha@ccc.ufcg.edu.br>
2025-02-04 00:09:20 -0300

    Adiciona config para serialização global usando DTO

--- src/main/java/br/com/foursales/ecommerce/api/FoursalesECommerceApiApplication.java
+++ src/main/java/br/com/foursales/ecommerce/api/FoursalesECommerceApiApplication.java
@@ -2,8 +2,12 @@ package br.com.foursales.ecommerce.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
+
+import static org.springframework.data.web.config.EnableSpringDataWebSupport.PageSerializationMode.VIA_DTO;
 
 @SpringBootApplication
+@EnableSpringDataWebSupport(pageSerializationMode = VIA_DTO)
 public class FoursalesECommerceApiApplication {
 
 	public static void main(String[] args) {
