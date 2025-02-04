1905de0634e67bd231b33c57ce29d70d6b76e828
Thalys <thalys.gadelha@ccc.ufcg.edu.br>
2025-02-04 00:11:07 -0300

    Cria regraDeNegocioException e trata exceções de forma específica no RestExceptionHandler

--- src/main/java/br/com/foursales/ecommerce/api/controller/exceptionHandler/RestExceptionHandler.java
+++ src/main/java/br/com/foursales/ecommerce/api/controller/exceptionHandler/RestExceptionHandler.java
@@ -1,20 +1,73 @@
 package br.com.foursales.ecommerce.api.controller.exceptionHandler;
 
+import br.com.foursales.ecommerce.api.exceptions.RegistroNaoEncontradoException;
+import br.com.foursales.ecommerce.api.exceptions.RegraDeNegocioException;
+import jakarta.validation.ConstraintViolationException;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.support.DefaultMessageSourceResolvable;
 import org.springframework.http.ProblemDetail;
 import org.springframework.security.core.AuthenticationException;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-import static org.springframework.http.HttpStatus.FORBIDDEN;
+import static java.util.stream.Collectors.joining;
+import static org.springframework.http.HttpStatus.BAD_REQUEST;
+import static org.springframework.http.HttpStatus.INTERNAL_SERVER_ERROR;
+import static org.springframework.http.HttpStatus.NOT_FOUND;
 import static org.springframework.http.HttpStatus.UNAUTHORIZED;
 
+@Slf4j
 @RestControllerAdvice
 public class RestExceptionHandler {
 
-    @ResponseStatus(value = FORBIDDEN)
+    @ExceptionHandler(value = Exception.class)
+    public ProblemDetail handleGeneralException(Exception e) {
+        log.error(e.getMessage(), e);
+        return ProblemDetail.forStatusAndDetail(INTERNAL_SERVER_ERROR, e.getMessage());
+    }
+
     @ExceptionHandler(value = AuthenticationException.class)
-    public ProblemDetail forbiddenException(AuthenticationException e) {
+    public ProblemDetail handleAuthenticationException(AuthenticationException e) {
+        log.error(e.getMessage(), e);
         return ProblemDetail.forStatusAndDetail(UNAUTHORIZED, e.getMessage());
+    }
+
+    @ExceptionHandler(value = MethodArgumentNotValidException.class)
+    public ProblemDetail handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
+        String message = e.getBindingResult().getAllErrors().stream()
+            .map(error -> {
+                if (error instanceof FieldError fieldError) {
+                    return fieldError.getField() + " " + fieldError.getDefaultMessage();
+                } else {
+                    return error.getDefaultMessage();
+                }
+            }).collect(joining(", "));
+        log.error(message, e);
+        return ProblemDetail.forStatusAndDetail(BAD_REQUEST, message);
+    }
+
+    @ExceptionHandler(value = ConstraintViolationException.class)
+    public ProblemDetail handleConstraintViolationException(ConstraintViolationException e) {
+        String message = e.getConstraintViolations().stream()
+            .map(cv -> cv.getPropertyPath() + " " +  cv.getMessage()).collect(joining(", "));
+        log.error(message, e);
+        return ProblemDetail.forStatusAndDetail(BAD_REQUEST, message);
+    }
+
+    @ExceptionHandler(value = RegraDeNegocioException.class)
+    public ProblemDetail handleRegraDeNegocioException(RegraDeNegocioException e) {
+        log.error(e.getMessage(), e);
+        return ProblemDetail.forStatusAndDetail(BAD_REQUEST, e.getMessage());
+    }
+
+    @ResponseStatus(value = NOT_FOUND)
+    @ExceptionHandler(value = RegistroNaoEncontradoException.class)
+    public ProblemDetail handleRegistroNaoEncontrado(RegistroNaoEncontradoException e) {
+        log.error(e.getMessage(), e);
+        return ProblemDetail.forStatusAndDetail(NOT_FOUND, e.getMessage());
     }
 }
---
+++ src/main/java/br/com/foursales/ecommerce/api/exceptions/RegraDeNegocioException.java
@@ -0,0 +1,8 @@
+package br.com.foursales.ecommerce.api.exceptions;
+
+public class RegraDeNegocioException extends Exception {
+
+    public RegraDeNegocioException(String msg) {
+        super(msg);
+    }
+}
