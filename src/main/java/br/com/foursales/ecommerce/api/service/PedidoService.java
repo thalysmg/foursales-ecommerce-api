48a710aa556368c05f1183e2f8a47659838621db
Thalys <thalys.gadelha@ccc.ufcg.edu.br>
2025-02-05 12:41:12 -0300

    Move envio de evento de criação do pedido para o service e remove do controller

--- src/main/java/br/com/foursales/ecommerce/api/service/PedidoService.java
+++ src/main/java/br/com/foursales/ecommerce/api/service/PedidoService.java
@@ -3,6 +3,7 @@ package br.com.foursales.ecommerce.api.service;
 import br.com.foursales.ecommerce.api.dto.PedidoDTO;
 import br.com.foursales.ecommerce.api.exceptions.RegistroNaoEncontradoException;
 import br.com.foursales.ecommerce.api.exceptions.RegraDeNegocioException;
+import br.com.foursales.ecommerce.api.kafka.producer.PedidoProducer;
 import br.com.foursales.ecommerce.api.model.Pedido;
 import br.com.foursales.ecommerce.api.model.PedidoProduto;
 import br.com.foursales.ecommerce.api.model.Produto;
@@ -14,17 +15,16 @@ import org.springframework.transaction.annotation.Transactional;
 import java.util.ArrayList;
 import java.util.List;
 
-import static java.util.stream.Collectors.toList;
-
 @Service
 @RequiredArgsConstructor
 public class PedidoService {
 
     private final PedidoRepository repository;
     private final ProdutoService produtoService;
+    private final PedidoProducer pedidoProducer;
 
     @Transactional(rollbackFor = Exception.class)
-    public String cadastrar(PedidoDTO pedidoDTO) throws RegraDeNegocioException, RegistroNaoEncontradoException {
+    public void cadastrar(PedidoDTO pedidoDTO) throws RegraDeNegocioException, RegistroNaoEncontradoException {
         List<Produto> produtos = produtoService.buscarPorIds(pedidoDTO.getIdsProdutos());
         if (produtos.stream().anyMatch(produto -> produto.getQtdEstoque().equals(0))) {
             throw new RegraDeNegocioException("Não foi possível realizar o pedidio. Um ou mais produtos estão esgotados em nosso estoque");
@@ -39,7 +39,8 @@ public class PedidoService {
             }
             pedidosProdutos.add(new PedidoProduto(produto, qtdProduto, produto.getPreco()));
         }
-        return repository.save(new Pedido(pedidosProdutos)).getId();
+        Pedido pedido = repository.save(new Pedido(pedidosProdutos));
+        pedidoProducer.enviarPedidoCriado(pedido.getId());
     }
 
     @Transactional(readOnly = true)
